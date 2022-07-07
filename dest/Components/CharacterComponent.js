668deeb9695fe517d35b8fdd0127d6a7b93bd431
Gene Keannu Miguel Kinazo <genekeannumiguelkinazo@Karlos-MacBook-Air.local>
2022-07-07 20:58:33 +0800

    tried

--- dest/Components/CharacterComponent.js
+++ dest/Components/CharacterComponent.js
@@ -2,13 +2,58 @@ import { CHARACTERS } from "../Variables/DataFile.js";
 export default class CharacterFormComponent extends HTMLElement {
     constructor() {
         super();
+        this._Character = CHARACTERS[0];
+        this._Level = 0;
+        this._Rank = 0;
         this.render();
     }
     render() {
         this.innerHTML = `
             <div>
-                <img src="${CHARACTERS.find(a => a.Name === this.getAttribute('character')).Image || CHARACTERS[0].Image}"
+                <img src="${this._Character.Image}">
+
+                <select id="Character">
+                    ${CHARACTERS.map(a => `<option value="${a.Name}">${a.Name}</option>`)}
+                </select>
+                <select id="Level">
+                    ${['1/20', '20/20', '20/40', '40/40', '40/50', '50/50', '50/60', '60/60', '60/70', '70/70', '70/80', '80/80', '80/90', '90/90']
+            .map((a, b) => `<option value="${b}">${a}</option>`)}
+                </select>
+                <select id="Rank">
+                    ${['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6'].map((a, b) => `<option value="${b}">${a}</option>`)}
+                </select>
             </div>
         `;
+        this.setAttribute('character', this.querySelector('#Character').value);
+        this.setAttribute('level', this.querySelector('#Level').value);
+        this.setAttribute('rank', this.querySelector('#Rank').value);
+    }
+    connectedCallback() {
+        this.querySelector('#Character').onchange = e => {
+            this.setAttribute('character', e.target.value);
+        };
+        this.querySelector('#Level').onchange = e => {
+            this.setAttribute('level', e.target.value);
+        };
+    }
+    disconnectedCallback() {
+        this.querySelector('#Character').onchange = null;
+        this.querySelector('#Level').onchange = null;
+    }
+    static get observedAttributes() {
+        return ['character', 'level', 'rank'];
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+            case 'character':
+                console.log(oldValue, newValue);
+                break;
+            case 'level':
+                console.log(oldValue, newValue);
+                break;
+            case 'rank':
+                console.log(oldValue, newValue);
+                break;
+        }
     }
 }
--- src/Components/CharacterComponent.ts
+++ src/Components/CharacterComponent.ts
@@ -1,7 +1,12 @@
+import { EventDispatcher } from "../Helpers/EventDispatcher.js";
 import { Character } from "../types"
 import { CHARACTERS } from "../Variables/DataFile.js"
 
 export default class CharacterFormComponent extends HTMLElement {
+    private _Character : Character = CHARACTERS[0];
+    private _Level: number = 0
+    private _Rank : number = 0
+    
     constructor() {
         super()
 
@@ -11,10 +16,64 @@ export default class CharacterFormComponent extends HTMLElement {
     private render(): void {
         this.innerHTML = `
             <div>
-                <img src="${
-                    (CHARACTERS.find(a => a.Name === this.getAttribute('character')) as Character ).Image || CHARACTERS[0].Image 
-                }"
+                <img src="${this._Character.Image}">
+
+                <select id="Character">
+                    ${
+                        CHARACTERS.map(a => `<option value="${a.Name}">${a.Name}</option>`)
+                    }
+                </select>
+                <select id="Level">
+                    ${
+                        ['1/20','20/20','20/40','40/40','40/50','50/50','50/60','60/60','60/70','70/70','70/80','80/80','80/90','90/90']
+                            .map((a,b) => `<option value="${b}">${a}</option>`)
+                    }
+                </select>
+                <select id="Rank">
+                    ${
+                        ['C0','C1','C2','C3','C4','C5','C6'].map((a,b) => `<option value="${b}">${a}</option>`)
+                    }
+                </select>
             </div>
         `
+
+        this.setAttribute('character', (this.querySelector('#Character') as HTMLSelectElement).value)
+        this.setAttribute('level', (this.querySelector('#Level') as HTMLSelectElement).value)
+        this.setAttribute('rank', (this.querySelector('#Rank') as HTMLSelectElement).value)
     }
+
+    connectedCallback() : void {
+        (this.querySelector('#Character') as HTMLSelectElement).onchange = e => {
+            this.setAttribute('character', (e.target as HTMLSelectElement).value)
+        }
+
+        (this.querySelector('#Level') as HTMLSelectElement).onchange = e => {
+            this.setAttribute('level', (e.target as HTMLSelectElement).value)
+        }
+    }
+
+    disconnectedCallback() : void {
+        (this.querySelector('#Character') as HTMLSelectElement).onchange = null;
+        (this.querySelector('#Level') as HTMLSelectElement).onchange = null;
+    }
+
+    static get observedAttributes() : string[] {
+        return ['character','level','rank']
+    }
+
+    attributeChangedCallback(name : string, oldValue : string, newValue : string) : void {
+        switch (name) {
+            case 'character':
+                console.log(oldValue, newValue)
+                break;
+            case 'level':
+                console.log(oldValue, newValue)
+                break;
+            case 'rank':
+                console.log(oldValue, newValue)
+                break;
+            
+        }
+    }
+    
 }
